3f9fd22d43a0e80df0b71660044066d41a9ba8a0
Camilo Santos <camilo@camilo-santos>
2019-04-14 15:02:32 -0300

    cada tweet coletado será gravado em tweets.db

--- main.py
+++ main.py
@@ -3,16 +3,35 @@ from __future__ import absolute_import, print_function
 import asyncio
 import configparser
 import json
+import sqlite3
 
 from tweepy import OAuthHandler, Stream
 from tweepy.streaming import StreamListener
 
 
+with sqlite3.connect('tweets.db') as conn:
+    cur = conn.cursor()
+    cur.execute(
+        'create table if not exists tweets (created_at text, track text, content text)')
+    conn.commit()
+
+
 class AssinanteTwitter(StreamListener):
+    def __init__(self, name):
+        super().__init__(self)
+        self.name = name
 
     def on_data(self, data):
         conteudoJSON = json.loads(data)
-        print(conteudoJSON["text"])
+        with sqlite3.connect('tweets.db') as conn:
+            cur = conn.cursor()
+            cur.execute(
+                'insert into tweets (created_at, track, content) values (?, ?, ?)',
+                (conteudoJSON['created_at'],
+                 self.name[0],
+                 conteudoJSON['text'])
+            )
+            conn.commit()
         return True
 
     def on_error(self, status):
@@ -30,7 +49,7 @@ async def stream(track: list):
     consumer_secret = config['auth']['consumer_secret']
     access_token = config['auth']['access_token']
     access_token_secret = config['auth']['access_token_secret']
-    assinante = AssinanteTwitter()
+    assinante = AssinanteTwitter(track)
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, assinante)
